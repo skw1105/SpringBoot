0eb239f225d516faa62d03116f3efb47380e59af
skw1105 <skw1105@gmail.com>
2021-08-23 02:36:19 +0900

    JPA

---
+++ src/main/java/com/utopia/springboot/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.utopia.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity //테이블과 연결될 클래스임을 나타냄
+public class Posts {
+
+    @Id //PK 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
