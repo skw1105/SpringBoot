3171d21dd16434a4d456b28c832af7826882663e
skw1105 <skw1105@gmail.com>
2021-12-06 02:14:41 +0900

    게시글 등록

--- src/main/java/com/utopia/springboot/controller/IndexController.java
+++ src/main/java/com/utopia/springboot/controller/IndexController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/") // Mustache 에 의해 문자열 반환 시 src/main/resources/templates가 앞에 붙고, .mustache가 뒤에 붙음
+    @GetMapping("/") // Mustache 에 의해 문자열 반환 시 "src/main/resources/templates"가 앞에 붙고, ".mustache"가 뒤에 붙음
     public String index() {
         return "index"; // src/main/resources/templates/index.mustache 로 전환되어 View Resolver가 처리하게 됨.
+    }
+
+    @GetMapping("/posts/save")
+    public String postSave() {
+        return "posts-save";
     }
 }
